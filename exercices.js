d3aecea5aa59cce74d755c081126a170335ac8f4
Dany Alonso <dany.alonso.pro@gmail.com>
2025-01-22 09:04:25 +0100

    Premier exercice

---
+++ exercices.js
@@ -0,0 +1,17 @@
+// const nom_sorcier = prompt("Comment te nommes-tu, sorcier ? 🧙‍♂️");
+// console.log("Le sorcier se nomme :" + nom_sorcier + " ! 🎉");
+
+/**
+ * Déclaration de variables
+ */
+const monnaie = "🪙";           // Déclaration de la monnaie pour les exercices
+
+// Exercice 1 - Définition de variables
+
+const store = "Poney Fringuant";
+let HealPot = 42;
+let PriceHealPot = 10;
+let StoreOpen = true;
+
+
+
