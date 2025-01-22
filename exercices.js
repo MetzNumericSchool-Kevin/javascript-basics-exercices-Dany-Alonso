0672a635be9af943485c688b283c332fa1aba554
Dany Alonso <dany.alonso.pro@gmail.com>
2025-01-22 09:36:25 +0100

    Troisième exercice

--- exercices.js
+++ exercices.js
@@ -1,4 +1,4 @@
-// const nom_sorcier = prompt("Comment te nommes-tu, sorcier ? 🧙‍♂️");
+const nom_sorcier = ("Archibald");
 // console.log("Le sorcier se nomme :" + nom_sorcier + " ! 🎉");
 
 /**
@@ -9,7 +9,7 @@ const monnaie = "🪙";                           // Déclaration de la monnaie 
 // Exercice 1 - Définition de variables
 
 const store = "Poney Fringuant";               // Déclaration du nom de la boutique en constante
-let HealPot = 42;                              // Déclaration d'une variable pour le nombre de potion de soin
+let QtyHealPot = 42;                              // Déclaration d'une variable pour le nombre de potion de soin
 let PriceHealPot = 10;                         // Déclaration d'une variable pour le prix d'une potion de soin
 let StoreOpen = true;                          // Déclaration d'une variable pour savoir si la boutique est ouverte ou non
 
@@ -26,4 +26,29 @@ if (StoreOpen) {
 
 // Exercice 3 - Affichage conditionnel avec switch
 
+const QueVeuxTu = parseInt(prompt("Bienvenue dans mon humble boutique Aventurier. Que veux-tu savoir ? 🤔\n- 1. Le nom de la boutique \n- 2. Le nom du Sorcier \n- 3. Le prix d'un potion de soin \n- 4. La quantité d'une potion de soin"));
 
+// console.log(typeof QueVeuxTu);
+
+/**
+ * Switch ... Case pour afficher dans la console une réponse celon le choix de l'utilisateur
+ */
+switch (QueVeuxTu) {
+    case 1:
+        console.log(store);
+        break;
+    case 2:
+        console.log(nom_sorcier);
+        break;
+    case 3:
+        console.log(PriceHealPot);
+        break;
+    case 4:
+        console.log(QtyHealPot);
+        break;
+    default:
+        console.log("Mh... Désolé aventurier, je ne comprends pas ce que tu souhaites. Refais ton choix ! 😕");
+        break;
+}
+
+// Exercice 4 - Calcul du prix total d'une commande de potion 🪙
